2a6f38800c00a15380001e008bc1d5499f39e62f
vertexofvortex <tbm92ru@gmail.com>
2023-07-23 18:03:18 +0300

    Update system base

---
+++ updates.lua
@@ -0,0 +1,20 @@
+local updates = {}
+
+-- Checks for an updates in a background
+function updates.check()
+    return false
+end
+
+-- Checks current version and new available version,
+-- checks if updates disabled by configuration file.
+-- Returns true if update should be installed and false if not
+function updates.checkShouldUpdate()
+    return false
+end
+
+-- Installs an update
+function updates.install()
+
+end
+
+return updates
